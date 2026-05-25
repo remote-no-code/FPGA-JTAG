374199684a27f659bb6fa767cf413610051cc1c9
Atharv Aggarwal <atharvaggarwal468@gmail.com>
2026-05-26 00:26:50 +0530

    Changed from 6 to 16 states

--- Task-1/jtag_tap.v
+++ Task-1/jtag_tap.v
@@ -8,14 +8,26 @@ module jtag_tap(
 	output reg tdo
 );
 
-parameter TEST_LOGIC_RESET = 0;
-parameter RUN_TEST_IDLE = 1;
-parameter SHIFT_IR = 2;
-parameter UPDATE_IR = 3;
-parameter SHIFT_DR = 4;
-parameter UPDATE_DR = 5;
+parameter TEST_LOGIC_RESET = 4'h0;
+parameter RUN_TEST_IDLE = 4'h1;
 
-reg[2:0] state;
+parameter SHIFT_IR = 4'h2;
+parameter UPDATE_IR = 4'h3;
+parameter SELECT_IR_SCAN = 4'h4; 
+parameter CAPTURE_IR = 4'h5;
+parameter EXIT1_IR = 4'h6;
+parameter PAUSE_IR = 4'h7;
+parameter EXIT2_IR = 4'h8;
+
+parameter SHIFT_DR = 4'h9;
+parameter UPDATE_DR = 4'hA;
+parameter SELECT_DR_SCAN = 4'hB; 
+parameter CAPTURE_DR = 4'hC;
+parameter EXIT1_DR = 4'hD;
+parameter PAUSE_DR = 4'hE;
+parameter EXIT2_DR = 4'hF;
+
+reg[3:0] state;
 
 parameter IDCODE = 4'b0001;
 parameter BYPASS = 4'b1111;
@@ -33,7 +45,7 @@ initial begin
 	ir = 4'b0000;
 	ir_shift = 4'b0000;
 	idcode = 32'h81262776;
-	dr_shift = 32'h81262776;
+	dr_shift = 32'h00000000;
 	bypass_bit = 1'b0;
 	tdo = 1'b0;
 	
@@ -55,25 +67,56 @@ always @(posedge tck or posedge trst) begin
 			
 			RUN_TEST_IDLE:
 				if(tms) 
-					state <= SHIFT_IR;
+					state <= SELECT_DR_SCAN;
 				else 
 					state <= RUN_TEST_IDLE;
 					
 			SHIFT_IR:
 				if(tms)
-					state <= UPDATE_IR;
+					state <= EXIT1_IR;
 				else
 					state <= SHIFT_IR;
 					
 			UPDATE_IR:
 				if(tms)
-					state <= SHIFT_DR;
+					state <= SELECT_DR_SCAN;
 				else 
 					state <= RUN_TEST_IDLE;
 				
+			SELECT_IR_SCAN:
+				if(tms) 
+					state <= TEST_LOGIC_RESET;
+				else
+					state <= CAPTURE_IR;
+					
+			CAPTURE_IR: 
+				if(tms)
+					state <= EXIT1_IR;
+				else
+					state <= SHIFT_IR;
+			
+			EXIT1_IR: 
+				if(tms)
+					state <= UPDATE_IR;
+				else
+					state <= PAUSE_IR;
+					
+			PAUSE_IR: 
+				if(tms)
+					state <= EXIT2_IR;
+				else
+					state <= PAUSE_IR;
+			
+			EXIT2_IR: 
+				if(tms)
+					state <= UPDATE_IR;
+				else
+					state <= SHIFT_IR;
+					
+					
 			SHIFT_DR:
 				if(tms)
-					state <= UPDATE_DR;
+					state <= EXIT1_DR;
 				else 
 					state <= SHIFT_DR;
 					
@@ -82,10 +125,48 @@ always @(posedge tck or posedge trst) begin
 					state <= TEST_LOGIC_RESET;
 				else
 					state <= RUN_TEST_IDLE;
+					
+			SELECT_DR_SCAN: 
+				if(tms)
+					state <= SELECT_IR_SCAN;
+				else
+					state <= CAPTURE_DR;
+					
+			CAPTURE_DR: 
+				if(tms)
+					state <= EXIT1_DR;
+				else
+					state <= SHIFT_DR;
+					
+			EXIT1_DR: 
+				if(tms)
+					state <= UPDATE_DR;
+				else
+					state <= PAUSE_DR;
+					
+			PAUSE_DR: 
+				if(tms)
+					state <= EXIT2_DR;
+				else
+					state <= PAUSE_DR;
+					
+			EXIT2_DR: 
+				if(tms)
+					state <= UPDATE_DR;
+				else
+					state <= SHIFT_DR;
+					
+			default:
+				state <= TEST_LOGIC_RESET;
 		endcase
 	end
 end
 
+always @(posedge tck) begin
+	
+	if(state == CAPTURE_IR)
+		ir_shift = 4'b0001;
+end
 
 always @(posedge tck) begin
 
@@ -105,6 +186,17 @@ always @(posedge tck) begin
 		
 end
 
+always @(posedge tck) begin
+	if( state == CAPTURE_DR) begin
+		case(ir)
+			IDCODE:
+				dr_shift <= idcode;
+			
+			BYPASS:
+				bypass_bit <= 1'b0;
+		endcase
+	end
+end
 
 always @(posedge tck) begin
 
@@ -123,12 +215,10 @@ always @(posedge tck) begin
 				bypass_bit <= tdi;
 			end
 			
-			default:
-				tdo <= 0;
-			
 		endcase
 	end
 end
+
 
 
 always @(*) begin
@@ -146,5 +236,4 @@ always @(*) begin
 end
 
 endmodule
-
 
